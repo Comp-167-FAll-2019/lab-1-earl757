dfa7f7beb3c391c57f9d24f22d04ca9c313a28dc
Earl757 <ewmcnair@aggies.ncat.edu>
2019-08-28 15:36:37 -0400

    finish the lab

--- Contacts/src/contacts/Contact.java
+++ Contacts/src/contacts/Contact.java
@@ -21,9 +21,25 @@ public class Contact {
         this.phoneNumber = phoneNumber;
         this.emailAddress = emailAddress;
     }
+     public String getFirstName(){
+         return firstName;
+         
+}
+     public void setFirstName( String firstName){
+         this.firstName=firstName;
+     }
+     public  String getLastName(){
+           return lastName;
+     }
+     public void setLastName(String lastName){
+         this.lastName=lastName;
+         
+     }
     
     
-    @Override
+    
+    
+    
     public String toString(){
         return firstName + " ," + lastName + " ," + phoneNumber + "," + emailAddress;
         
